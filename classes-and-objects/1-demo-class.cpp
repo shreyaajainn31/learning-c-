31f0ac4a9764027a48de6ae49333c0bbcf0e9027
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-18 22:05:56 -0400

    Create 1-demo-class.cpp

---
+++ classes-and-objects/1-demo-class.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class myClass{
+  
+  public:
+    void print(){
+        cout<<"hello class";    
+    }
+    
+};
+
+int main()
+{
+    
+    myClass obj;
+    
+    obj.print();
+    
+    return 0;
+}
