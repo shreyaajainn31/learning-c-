7ca3af93c56d4794254e427673bc8221a5925784
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-18 21:55:57 -0400

    to check if a number is <,>,=

---
+++ variables/4-if-conditions-on-variable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=4,b=5;
+    
+    if(a<b){
+        cout<<"less than";
+    }    
+    else if(a == b){
+        cout<<"equal";
+    }
+    else{
+        cout<<"greater than";
+    }
+
+    return 0;
+}
