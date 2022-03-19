27935b8dec6e4ec67fe1f6d4435dce276c7aac1f
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-18 21:51:09 -0400

    taking integer input

---
+++ variables/2-taking-variable-input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    
+    cout<<"Enter numbers to sum:"<<endl;
+    
+    cin>>a>>b;
+
+    cout<<"The sum is: "<<a+b;
+    return 0;
+}
