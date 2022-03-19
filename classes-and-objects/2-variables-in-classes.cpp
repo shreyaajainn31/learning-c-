3cc873952f02aa0ac4d7eadba3dbe67277439a5b
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-18 22:14:13 -0400

    Create 2-variables-in-classes.cpp

---
+++ classes-and-objects/2-variables-in-classes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class myClass{
+  
+  public:
+    string name;
+    
+};
+
+int main()
+{
+    
+    myClass obj;
+    
+    obj.name = "Shreya";
+    cout<<obj.name;
+    return 0;
+}
